42b73e12187690eaa784f51ac90350fceb149ea5
Lewis Lakerink <llakerink@swin.edu.au>
2022-06-20 22:38:07 +0000

    Publication management
    Adds publication management to the API. Allows creating, querying and deleting publications.
    
    Creation/Deletion is reserved for publication managers (defined on the backend), querying is available to the public.
    
    Some previous tests for Keywords were refactored to use a new graphql query comparison.
    
    Approved-by: TomReichardt <thomas.reichardt@students.mq.edu.au>
    
    Merged by: Lewis Lakerink <llakerink@swin.edu.au>
    
    See merge request https://gitlab.com/CAS-eResearch/GWDC/code/gwlandscape_python/-/merge_requests/3

---
+++ gwlandscape_python/publication_type.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Publication:
+    id: str
+    author: str
+    published: bool
+    title: str
+    year: int
+    journal: str
+    journal_doi: str
+    dataset_doi: str
+    description: str
+    public: bool
+    download_link: str
+    arxiv_id: str
+    creation_time: str
+    keywords: list
--- gwlandscape_python/tests/test_gwlandscape_python.py
+++ gwlandscape_python/tests/test_gwlandscape_python.py
@@ -2,6 +2,8 @@ import pytest
 
 from gwlandscape_python import GWLandscape
 from gwlandscape_python.keyword_type import Keyword
+from gwlandscape_python.publication_type import Publication
+from gwlandscape_python.tests.utils import compare_graphql_query
 
 
 @pytest.fixture
@@ -48,6 +50,65 @@ def create_keyword_request(setup_gwl_request):
     return gwl, mr
 
 
+@pytest.fixture
+def create_publication_request(setup_gwl_request):
+    response_data = [
+        {
+            "add_publication": {
+                "id": "Q29tcGFzUHVibGljYXRpb25Ob2RlOjUw"
+            }
+        },
+        {
+            'compas_publications': {
+                'edges': [
+                    {
+                        'node': {
+                            'id': 'Q29tcGFzUHVibGljYXRpb25Ob2RlOjUw',
+                            'author': 'my author',
+                            'published': True,
+                            'title': 'my publication',
+                            'year': 1234,
+                            'journal': 'journal',
+                            'journal_doi': 'journal doi',
+                            'dataset_doi': 'dataset doi',
+                            'creation_time': '2022-06-20T02:12:59.459297+00:00',
+                            'description': 'my description',
+                            'public': True,
+                            'download_link': 'my download link',
+                            'arxiv_id': 'an arxiv id',
+                            'keywords': {
+                                'edges': [
+                                    {
+                                        'node': {
+                                            'id': 'kw_id_1',
+                                            'tag': 'keyword1'
+                                        }
+                                    },
+                                    {
+                                        'node': {
+                                            'id': 'kw_id_2',
+                                            'tag': 'keyword2'
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ]
+
+    gwl, mr = setup_gwl_request
+
+    def mock_request(*args, **kwargs):
+        return response_data.pop(0)
+
+    mr.side_effect = mock_request
+
+    return gwl, mr
+
+
 def test_create_keyword(create_keyword_request):
     gw, mock_request = create_keyword_request
 
@@ -56,25 +117,28 @@ def test_create_keyword(create_keyword_request):
     assert keyword.id == 'S2V5d29yZE5vZGU6MzA='
     assert keyword.tag == 'my_tag'
 
-    mock_request.mock_calls[0].assert_called_with(
+    assert compare_graphql_query(
+        mock_request.mock_calls[0].args[0],
         """
             mutation AddKeywordMutation($input: AddKeywordMutationInput!) {
                 addKeyword(input: $input) {
                     id
                 }
             }
-        """,
-        {
-            'input': {
-                'tag': "my_tag"
-            }
-        }
+        """
     )
 
-    mock_request.mock_calls[1].assert_called_with(
+    assert mock_request.mock_calls[0].args[1] == {
+        'input': {
+            'tag': 'my_tag'
+        }
+    }
+
+    assert compare_graphql_query(
+        mock_request.mock_calls[1].args[0],
         """
             query {
-                keywords (tag: "my_tag") {
+                keywords (id: "S2V5d29yZE5vZGU6MzA=") {
                     edges {
                         node {
                             id
@@ -277,6 +341,349 @@ def test_delete_keyword(setup_gwl_request):
         {
             'input': {
                 'id': keyword.id
+            }
+        }
+    )
+
+
+def test_create_publication(create_publication_request):
+    gw, mock_request = create_publication_request
+
+    kws = [
+        Keyword('kw_id_1', 'keyword1'),
+        Keyword('kw_id_2', 'keyword2'),
+    ]
+
+    publication = gw.create_publication(
+        'my author',
+        'my publication',
+        'an arxiv id',
+        published=True,
+        year=1234,
+        journal="journal",
+        journal_doi="journal doi",
+        dataset_doi="dataset doi",
+        description='my description',
+        public=True,
+        download_link='my download link',
+        keywords=kws
+    )
+
+    assert publication.id == 'Q29tcGFzUHVibGljYXRpb25Ob2RlOjUw'
+    assert publication.author == 'my author'
+    assert publication.title == 'my publication'
+    assert publication.arxiv_id == 'an arxiv id'
+    assert publication.published is True
+    assert publication.year == 1234
+    assert publication.journal == "journal"
+    assert publication.journal_doi == "journal doi"
+    assert publication.dataset_doi == "dataset doi"
+    assert publication.description == 'my description'
+    assert publication.public is True
+    assert publication.download_link == 'my download link'
+    assert publication.keywords == kws
+
+    assert compare_graphql_query(
+        mock_request.mock_calls[0].args[0],
+        """
+            mutation AddPublicationMutation($input: AddPublicationMutationInput!) {
+                addPublication(input: $input) {
+                    id
+                }
+            }
+        """
+    )
+
+    assert mock_request.mock_calls[0].args[1] == {
+        'input': {
+            'author': 'my author',
+            'title': 'my publication',
+            'arxiv_id': 'an arxiv id',
+            'published': True,
+            'year': 1234,
+            'journal': "journal",
+            'journal_doi': "journal doi",
+            'dataset_doi': "dataset doi",
+            'description': 'my description',
+            'public': True,
+            'download_link': 'my download link',
+            'keywords': ['kw_id_1', 'kw_id_2']
+        }
+    }
+
+    print(mock_request.mock_calls[1].args[0])
+
+    assert compare_graphql_query(
+        mock_request.mock_calls[1].args[0],
+        """
+            query {
+                compasPublications (id: "Q29tcGFzUHVibGljYXRpb25Ob2RlOjUw") {
+                    edges {
+                        node {
+                            id
+                            author
+                            published
+                            title
+                            year
+                            journal
+                            journalDoi
+                            datasetDoi
+                            creationTime
+                            description
+                            public
+                            downloadLink
+                            arxivId
+                            keywords {
+                                edges {
+                                    node {
+                                        id
+                                        tag
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        """
+    )
+
+
+def get_publication(gwl, mock_request, author=None, title=None, _id=None):
+    mock_request.return_value = {
+        'compas_publications': {
+            'edges': [
+                {
+                    'node': {
+                        'id': 'Q29tcGFzUHVibGljYXRpb25Ob2RlOjUw',
+                        'author': 'my author',
+                        'published': True,
+                        'title': 'my publication',
+                        'year': 1234,
+                        'journal': 'journal',
+                        'journal_doi': 'journal doi',
+                        'dataset_doi': 'dataset doi',
+                        'creation_time': '2022-06-20T02:12:59.459297+00:00',
+                        'description': 'my description',
+                        'public': True,
+                        'download_link': 'my download link',
+                        'arxiv_id': 'an arxiv id',
+                        'keywords': {
+                            'edges': [
+                                {
+                                    'node': {
+                                        'id': 'kw_id_1',
+                                        'tag': 'keyword1'
+                                    }
+                                },
+                                {
+                                    'node': {
+                                        'id': 'kw_id_2',
+                                        'tag': 'keyword2'
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                }
+            ]
+        }
+    }
+
+    pub = gwl.get_publications(author, title, _id)
+    assert len(pub) == 1
+
+    publication = pub[0]
+
+    kws = [
+        Keyword('kw_id_1', 'keyword1'),
+        Keyword('kw_id_2', 'keyword2'),
+    ]
+
+    assert publication.id == 'Q29tcGFzUHVibGljYXRpb25Ob2RlOjUw'
+    assert publication.author == 'my author'
+    assert publication.title == 'my publication'
+    assert publication.arxiv_id == 'an arxiv id'
+    assert publication.published is True
+    assert publication.year == 1234
+    assert publication.journal == "journal"
+    assert publication.journal_doi == "journal doi"
+    assert publication.dataset_doi == "dataset doi"
+    assert publication.description == 'my description'
+    assert publication.public is True
+    assert publication.download_link == 'my download link'
+    assert publication.keywords == kws
+
+
+def test_get_publication_author(setup_gwl_request):
+    gwl, mock_request = setup_gwl_request
+
+    get_publication(gwl, mock_request, author='test_author')
+
+    assert compare_graphql_query(
+        mock_request.mock_calls[0].args[0],
+        """
+            query {
+                compasPublications (author_Icontains: "test_author") {
+                    edges {
+                        node {
+                            id
+                            author
+                            published
+                            title
+                            year
+                            journal
+                            journalDoi
+                            datasetDoi
+                            creationTime
+                            description
+                            public
+                            downloadLink
+                            arxivId
+                            keywords {
+                                edges {
+                                    node {
+                                        id
+                                        tag
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        """
+    )
+
+
+def test_get_publication_title(setup_gwl_request):
+    gwl, mock_request = setup_gwl_request
+
+    get_publication(gwl, mock_request, title='test_title')
+
+    assert compare_graphql_query(
+        mock_request.mock_calls[0].args[0],
+        """
+            query {
+                compasPublications (title_Icontains: "test_title") {
+                    edges {
+                        node {
+                            id
+                            author
+                            published
+                            title
+                            year
+                            journal
+                            journalDoi
+                            datasetDoi
+                            creationTime
+                            description
+                            public
+                            downloadLink
+                            arxivId
+                            keywords {
+                                edges {
+                                    node {
+                                        id
+                                        tag
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        """
+    )
+
+
+def test_get_publication_author_title(setup_gwl_request):
+    gwl, mock_request = setup_gwl_request
+
+    get_publication(gwl, mock_request, author='test_author', title='test_title')
+
+    assert compare_graphql_query(
+        mock_request.mock_calls[0].args[0],
+        """
+            query {
+                compasPublications (title_Icontains: "test_title", author_Icontains: "test_author") {
+                    edges {
+                        node {
+                            id
+                            author
+                            published
+                            title
+                            year
+                            journal
+                            journalDoi
+                            datasetDoi
+                            creationTime
+                            description
+                            public
+                            downloadLink
+                            arxivId
+                            keywords {
+                                edges {
+                                    node {
+                                        id
+                                        tag
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        """
+    )
+
+
+def test_get_publication_author_title_id(setup_gwl_request):
+    gwl, mock_request = setup_gwl_request
+
+    with pytest.raises(AssertionError):
+        get_publication(gwl, mock_request, author='test_author', title='test_title', _id='not_gonna_work')
+
+
+def test_delete_publication(setup_gwl_request):
+    gwl, mock_request = setup_gwl_request
+
+    mock_request.return_value = {
+        "delete_publication": {
+            "result": True
+        }
+    }
+
+    publication = Publication(**{
+        'id': 'Q29tcGFzUHVibGljYXRpb25Ob2RlOjUw',
+        'author': 'my author',
+        'published': True,
+        'title': 'my publication',
+        'year': 1234,
+        'journal': 'journal',
+        'journal_doi': 'journal doi',
+        'dataset_doi': 'dataset doi',
+        'creation_time': '2022-06-20T02:12:59.459297+00:00',
+        'description': 'my description',
+        'public': True,
+        'download_link': 'my download link',
+        'arxiv_id': 'an arxiv id',
+        'keywords': []
+    })
+
+    gwl.delete_publication(publication)
+
+    mock_request.assert_called_with(
+        """
+            mutation DeletePublicationMutation($input: DeletePublicationMutationInput!) {
+                deletePublication(input: $input) {
+                    result
+                }
+            }
+        """,
+        {
+            'input': {
+                'id': publication.id
             }
         }
     )
